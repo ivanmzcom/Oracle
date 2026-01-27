439c58d048a77522bcb2666adc3e6b21bdba209d
Iván Moreno Zambudio <ivan.moreno@ticarum.es>
2026-01-27 22:16:38 +0100

    Initial Commit

---
+++ Trakt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trakt
+//
+//  Created by Iván Moreno Zambudio on 27/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trakt/TraktApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraktApp.swift
+//  Trakt
+//
+//  Created by Iván Moreno Zambudio on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TraktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
